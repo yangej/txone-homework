6c48e8fc56e47405b87f92ac194f5ec7d92c5477
ejyang <yangej0218@gmail.com>
2024-08-04 04:00:17 +0800

    feat(modules/weather): finish weather view ui

--- src/modules/weather/components/WeatherView.tsx
+++
@@ -1,7 +0,0 @@
-import PageView from '../../app/components/PageView';
-
-const WeatherView = () => {
-  return <PageView title="Today's Weather">Weather</PageView>;
-};
-
-export default WeatherView;
---
+++ src/modules/weather/components/WeatherView/WeatherView.tsx
@@ -0,0 +1,35 @@
+import { Card } from '@mui/material';
+
+import PageView from '../../../app/components/PageView';
+import { fromKelvinToCelsius } from '../../utils';
+import WeatherCardFooter from './WeatherCardFooter';
+import WeatherCardHeader from './WeatherCardHeader';
+import WeatherCardMain from './WeatherCardMain';
+
+const WeatherView = () => {
+  return (
+    <PageView title="Today's Weather">
+      <Card
+        variant="outlined"
+        className="flex flex-col justify-between p-8"
+        sx={{ maxWidth: '542px' }}
+      >
+        <WeatherCardHeader location="Taipei" timestamp={1661870592} />
+        <WeatherCardMain
+          temperature={fromKelvinToCelsius(298.48)}
+          main="Rain"
+          description="moderate rain"
+        />
+        <WeatherCardFooter
+          humidity={64}
+          temperatures={{
+            min: fromKelvinToCelsius(297.56),
+            max: fromKelvinToCelsius(300.05),
+          }}
+        />
+      </Card>
+    </PageView>
+  );
+};
+
+export default WeatherView;
