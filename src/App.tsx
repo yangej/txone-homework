093fbe0fef41cb4c0c2a1a37e6e43dfd64b22e97
ejyang <yangej0218@gmail.com>
2024-08-03 16:49:33 +0800

    feat(App): apply temp views

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,36 @@
+import { Suspense, lazy } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import AppLayout from './modules/app/components/AppLayout';
 import { isValidRoutePath } from './modules/app/utils';
+
+const WeatherView = lazy(
+  () => import('./modules/weather/components/WeatherView'),
+);
+const PopulationView = lazy(
+  () => import('./modules/population/components/PopulationView'),
+);
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path={isValidRoutePath('/')} element={<AppLayout />}>
-          <Route path={isValidRoutePath('/weather')} element={<>Weather</>} />
+          <Route
+            path={isValidRoutePath('/weather')}
+            element={
+              <Suspense fallback={<div>Page Loading...</div>}>
+                <WeatherView />
+              </Suspense>
+            }
+          />
           <Route
             path={isValidRoutePath('/population')}
-            element={<>Population</>}
+            element={
+              <Suspense fallback={<div>Page Loading...</div>}>
+                <PopulationView />
+              </Suspense>
+            }
           />
           <Route path="*" element={<>Not Found</>} />
         </Route>
