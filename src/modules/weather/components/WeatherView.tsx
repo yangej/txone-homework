abcfb2a819e60d8d174ab62f4db4a06d3067900a
ejyang <yangej0218@gmail.com>
2024-08-03 16:44:30 +0800

    feat(modules/weather): add temp weather view

---
+++ src/modules/weather/components/WeatherView.tsx
@@ -0,0 +1,7 @@
+import PageView from '../../app/components/PageView';
+
+const WeatherView = () => {
+  return <PageView title="Today's Weather">Weather</PageView>;
+};
+
+export default WeatherView;
