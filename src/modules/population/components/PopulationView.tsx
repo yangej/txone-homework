bd2f10a416b6faaf2fd3830aec69da8a7e487486
ejyang <yangej0218@gmail.com>
2024-08-03 16:46:50 +0800

    feat(modules/population): add temp population view

---
+++ src/modules/population/components/PopulationView.tsx
@@ -0,0 +1,7 @@
+import PageView from '../../app/components/PageView';
+
+const PopulationView = () => {
+  return <PageView title="Population">Population</PageView>;
+};
+
+export default PopulationView;
