ab39d1bd1845c398430f19e47b6d2aaa2ea21a90
ejyang <yangej0218@gmail.com>
2024-08-04 23:29:25 +0800

    feat(modules/weather): support default values in search bar

--- src/modules/weather/components/WeatherSearchBar.tsx
+++ src/modules/weather/components/WeatherSearchBar.tsx
@@ -4,10 +4,17 @@ import { FormEventHandler } from 'react';
 type FormDataValue = Record<string, string>;
 
 export interface WeatherSearchBarProps {
+  defaultValues?: {
+    city?: string;
+    country?: string;
+  };
   onSubmit(value: FormDataValue): void;
 }
 
-const WeatherSearchBar = ({ onSubmit }: WeatherSearchBarProps) => {
+const WeatherSearchBar = ({
+  defaultValues = {},
+  onSubmit,
+}: WeatherSearchBarProps) => {
   const handleSubmit: FormEventHandler<HTMLFormElement> = (e) => {
     e.preventDefault();
 
@@ -18,24 +25,33 @@ const WeatherSearchBar = ({ onSubmit }: WeatherSearchBarProps) => {
 
   return (
     <form onSubmit={handleSubmit}>
-      <Stack direction="row" gap="14px" alignItems="center" maxWidth="542px">
-        <label htmlFor="city">City</label>
-        <TextField id="city" name="city" size="small" sx={{ width: '132px' }} />
-        <label htmlFor="country">Country</label>
-        <TextField
-          id="country"
-          name="country"
-          size="small"
-          sx={{ width: '132px' }}
-        />
-        <Button
-          type="submit"
-          variant="contained"
-          className="flex-grow"
-          color="primary"
-        >
-          Search
-        </Button>
+      <Stack direction="column" gap="12px">
+        <Stack direction="row" gap="14px" alignItems="center" maxWidth="542px">
+          <label htmlFor="city">City</label>
+          <TextField
+            id="city"
+            name="city"
+            size="small"
+            sx={{ width: '132px', backgroundColor: 'white' }}
+            defaultValue={defaultValues?.city}
+          />
+          <label htmlFor="country">Country</label>
+          <TextField
+            id="country"
+            name="country"
+            size="small"
+            sx={{ width: '132px', backgroundColor: 'white' }}
+            defaultValue={defaultValues?.country}
+          />
+          <Button
+            type="submit"
+            variant="contained"
+            className="flex-grow"
+            color="primary"
+          >
+            Search
+          </Button>
+        </Stack>
       </Stack>
     </form>
   );
